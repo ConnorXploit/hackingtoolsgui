0a240ef510177e7e1497c4d7093ef497154a065c
Unknown <cgehminecraft@gmail.com>
2019-04-06 01:28:55 +0200

    Config Loaded

--- core/views.py
+++ core/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-
+import hackingtools
 # Create your views here.
 
 def home(request):
-    return render(request, 'core/index.html', {})
+    modules_and_params = hackingtools.getModulesJSON()
+
+    modules_all = {}
+    categories = []
+
+    for mod in modules_and_params:
+        if not mod.split('.')[2] in categories:
+            categories.append(mod.split('.')[2])
+        modules_all[mod.split('.')[3]] = modules_and_params[mod]
+    modules_names = hackingtools.getModulesNames()
+    return render(request, 'core/index.html', { 'modules':modules_names, 'categories':categories, 'modules_all':modules_all })
+
+def module(request):
+    pass
