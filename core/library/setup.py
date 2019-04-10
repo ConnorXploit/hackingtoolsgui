c9aa0cf41fe0fba6d409eff9fadb45628545ea85
Unknown <cgehminecraft@gmail.com>
2019-04-10 16:43:31 +0200

    0.6.7

--- core/library/setup.py
+++ core/library/setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(name='hackingtools',
-        version='0.6.4',
+        version='0.6.7',
         description='All Hacking Tools in this Python with Manually Created Modules',
         url='http://github.com/ConnorXploit/hackingtools-py',
         author='Connor',
