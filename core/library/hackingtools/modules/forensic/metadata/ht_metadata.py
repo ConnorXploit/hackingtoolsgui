6eef57526aca24bfe045311bb72badc16f276b93
Unknown <cgehminecraft@gmail.com>
2019-07-10 19:37:32 +0200

    Metadata Forensics Modules

---
+++ core/library/hackingtools/modules/forensic/metadata/ht_metadata.py
@@ -0,0 +1,35 @@
+from hackingtools.core import Logger
+import hackingtools as ht
+
+from PIL import Image
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+class StartModule():
+
+	def __init__(self):
+		Logger.printMessage(message='ht_metadata loaded', debug_module=True)
+		pass
+
+	def help(self):
+		Logger.printMessage(message=ht.getFunctionsNamesFromModule('ht_metadata'))
+
+	def get_image_exif(self, filename):
+		try:
+			img_file = Image.open(filename)
+			img_file.verify()
+			info = img_file._getexif()
+			return info
+		except:
+			pass
+		return
+
+	def get_pdf_exif(self, pdf_file):
+		info = ''
+		data = {}
+		with open(pdf_file, 'rb') as f:
+			pdf = PdfFileReader(f)
+			info = pdf.getDocumentInfo()
+			number_of_pages = pdf.getNumPages()
+		for a in info:
+			data[a] = info[a]
+		return data
