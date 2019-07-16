7985421fe0b601464da3695e4a2fa6fab2c7283a
Connor <cgehminecraft@gmail.com>
2019-07-16 22:30:30 +0200

    Add files via upload

--- install_gui.sh
+++ install_gui.sh
@@ -1,14 +1,9 @@
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
-   exit 1
-fi
-sudo apt update -y
+sudo apt update
 conda env create -f=environment.yml
 conda activate ht
-sudo apt-get install python-dev
-sudo apt-get install libjpeg-dev
-sudo apt-get install libtiff-dev libjpeg-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl-dev tk-dev python-tk
+sudo apt-get install python-dev -y
+sudo apt-get install libjpeg-dev -y
+sudo apt-get install libtiff-dev libjpeg-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl-dev tk-dev python-tk -y
 pip install -r requirements.txt
-clear
 echo "All is done :) Now type:"
 echo "python manage.py runserver"
