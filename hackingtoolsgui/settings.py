473034fece6b3415a25b683e92f8085ecd8e4bfc
ConnorHack <cgehminecraft@gmail.com>
2019-10-18 02:51:26 +0200

    Changes for static in Heroku

--- hackingtoolsgui/settings.py
+++ hackingtoolsgui/settings.py
@@ -119,8 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+PROJECT_ROOT = os.path.split(os.path.dirname(os.path.abspath(__file__)))[0]
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'core', 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
