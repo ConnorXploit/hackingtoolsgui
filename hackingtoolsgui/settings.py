caf7ec8a3f76300bd67806b554e7608446e5405c
ConnorHack <cgehminecraft@gmail.com>
2019-10-18 02:54:29 +0200

    Another try

--- hackingtoolsgui/settings.py
+++ hackingtoolsgui/settings.py
@@ -119,8 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-PROJECT_ROOT = os.path.split(os.path.dirname(os.path.abspath(__file__)))[0]
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'core', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'core', 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
