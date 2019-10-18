8cf10f5ea72f6fb375c431df0a154c82da27819d
ConnorHack <cgehminecraft@gmail.com>
2019-10-18 16:42:56 +0200

    Cant remove in server? Trying something new

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn hackingtoolsgui.wsgi
+web: python server.py
--- core/library/hackingtools/core/Repositories.py
+++ core/library/hackingtools/core/Repositories.py
@@ -58,6 +58,7 @@ def removeModule(server, moduleName):
         category_folder = os.path.join(path, 'modules', category)
         if os.path.isdir(category_folder):
             module_folder = os.path.join(category_folder, moduleName)
+            print(module_folder)
             if os.path.isdir(module_folder):
                 shutil.rmtree(module_folder)
 
