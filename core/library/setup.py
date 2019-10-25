40df25373fb322dce4c8f3ac6b6348b1c059bbb7
ConnorHack <cgehminecraft@gmail.com>
2019-10-25 22:50:02 +0200

    This is horrible...

--- core/library/setup.py
+++ core/library/setup.py
@@ -32,7 +32,7 @@ def checkPackages():
 checkPackages()
 
 setup(name='hackingtools',
-        version='1.5.0',
+        version='1.5.1',
         description='All Hacking Tools in this Python with Manually Created Modules',
         url='http://github.com/ConnorXploit/hackingtools-py',
         author='Connor',
