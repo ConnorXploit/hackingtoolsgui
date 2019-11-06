9f8b1acdf575238d2b7f98bfe98d1296b6f6b589
ConnorHack <cgehminecraft@gmail.com>
2019-11-07 00:10:57 +0100

    New version pypi 1.7.0

--- core/library/setup.py
+++ core/library/setup.py
@@ -32,7 +32,7 @@ def checkPackages():
 checkPackages()
 
 setup(name='hackingtools',
-        version='1.6.9',
+        version='1.7.0',
         description='All Hacking Tools in this Python with Manually Created Modules',
         url='http://github.com/ConnorXploit/hackingtools-py',
         author='Connor',
