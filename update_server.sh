eb3f13b80961ec04eb12cedc8886e9e94a2bc714
ConnorHack <cgehminecraft@gmail.com>
2019-12-29 13:56:06 +0100

    Updater

--- update_server.sh
+++ update_server.sh
@@ -1,4 +1,5 @@
-sudo git fetch --all
-sudo git pull origin master
+git fetch --all
+git reset --hard origin/master
+git pull origin master
 sudo pip3 install -r requirements.txt -U
 sudo pip3 install hackingtools -U
