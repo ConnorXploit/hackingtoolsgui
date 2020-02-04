9f4a09993dd6c603317d49fa1b627e13bc38a055
Luigy0x18 <lgilbernabe@apache.org>
2020-02-04 02:51:04 +0100

    testing

---
+++ core/library/hackingtools/modules/reversing/radare2/ht_radare2.py
@@ -0,0 +1,27 @@
+from hackingtools.core import Logger, Utils, Config
+if Utils.amIdjango(__name__):
+	from .library.core import hackingtools as ht
+else:
+	import hackingtools as ht
+import os
+import r2pipe
+
+config = Config.getConfig(parentKey='modules', key='ht_radare2')
+output_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'output'))
+
+class StartModule():
+
+
+	def __init__(self):
+		self._main_gui_func_ = ''
+
+	def help(self):
+		Logger.printMessage(message=ht.getFunctionsNamesFromModule('ht_radare2'), debug_module=True)
+
+	def getImports(self, filemon):
+		try:
+			r = r2pipe.open(filemon)
+			# saca la tabla de dll importadas en json
+			return r.cmd('iij')
+		except Exception as e:
+			return str(e)
