05ab10505b010f50ad0672f94e648f831b5dbdf7
Connor <connordev@protonmail.com>
2020-09-10 22:32:42 +0200

    Start Server gunicorn fix

--- start-server.sh
+++ start-server.sh
@@ -2,5 +2,5 @@
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
     (cd hackingtools; python manage.py createsuperuser --no-input; cd ..)
 fi
-(gunicorn hackingtoolsgui.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) &
+(gunicorn hackingtoolsgui/wsgi.py --user www-data --bind 0.0.0.0:8010 --workers 3) &
 nginx -g "daemon off;"
