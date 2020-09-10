7fb0fe812415ee97e190c25cc21ef36dcc4b07ce
Connor <connordev@protonmail.com>
2020-09-10 21:25:35 +0200

    DockerFile

---
+++ start-server.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
+    (cd hackingtools; python manage.py createsuperuser --no-input)
+fi
+(cd hackingtools; gunicorn hackingtoolsgui.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) &
+nginx -g "daemon off;"
